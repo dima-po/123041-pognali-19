492f966fca3016b4e024702f5d192c142e2b19bf
Dima Po <dima.djunaev@gmail.com>
2020-04-27 14:18:41 +0500

    Задание №3. Главная страница

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,25 @@
 console.log("Привет, ты смотришь проект Погнали");
+
+
+let header = document.querySelector('.header');
+let nav = document.querySelector('.header__nav-btn');
+let headerNavBox = document.querySelector('.header__nav-box');
+let logoImg = document.querySelector('.logo__img');
+
+console.log(logoImg.getAttribute('src'));
+
+nav.addEventListener('click', function(e) {
+  nav.classList.toggle('header__nav-btn--active');
+  headerNavBox.classList.toggle('header__nav-box--opened');
+  header.classList.toggle('header--bg-white');
+
+  setInterval(function(){
+    if(nav.classList.contains('header__nav-btn--active')) {
+      logoImg.setAttribute('src', 'img/logo-mobile-blue@1x.png');
+    }
+
+    else {
+      logoImg.setAttribute('src', 'img/logo-mobile-white@1x.png');
+    }
+  }, 100)
+});
