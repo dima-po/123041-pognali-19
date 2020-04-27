c7824aaf57962cfbe885ab66005f67ce466da9d5
Dima Po <dima.djunaev@gmail.com>
2020-04-27 15:08:45 +0500

    Всплывающее окно

--- source/js/main.js
+++ source/js/main.js
@@ -1,12 +1,16 @@
 console.log("Привет, ты смотришь проект Погнали");
 
-
+// Content
 let header = document.querySelector('.header');
 let nav = document.querySelector('.header__nav-btn');
 let headerNavBox = document.querySelector('.header__nav-box');
 let logoImg = document.querySelector('.logo__img');
 
-console.log(logoImg.getAttribute('src'));
+// Popup
+let popup = document.querySelector('.popup');
+let popupOpenBtn = document.querySelector('.tariffs__btn');
+let popupCloseBtn = document.querySelector('.popup__close-btn');
+
 
 nav.addEventListener('click', function(e) {
   nav.classList.toggle('header__nav-btn--active');
@@ -22,4 +26,13 @@ nav.addEventListener('click', function(e) {
       logoImg.setAttribute('src', 'img/logo-mobile-white@1x.png');
     }
   }, 100)
+});
+
+popupOpenBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  popup.classList.add('popup--opened');
+});
+
+popupCloseBtn.addEventListener('click', function(e) {
+  popup.classList.remove('popup--opened');
 });
